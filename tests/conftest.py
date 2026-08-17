d16ce5395ba1ce0cc57317dd3c74ebb48fc1eee6
IIsmail15 <israa.ismail8i4@gmail.com>
2026-08-17 23:15:26 +0100

    test db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,5 @@
-#shared fixtures for tests 
+from dotenv import load_dotenv
 
-from data.generate_data import generate_rentals, generate_drives, generate_insurances, generate_payments
-from sqlalchemy import create_engine
-import os
-import pytest
 
-@pytest.fixture(scope="session", autouse=True)
-def setup_database():
-    # Generate data for all tables before running tests
-    generate_rentals()
-    generate_drives()
-    generate_insurances()
-    generate_payments() 
+load_dotenv(".env.test")  # Load environment variables from .env.test file for testing
 
